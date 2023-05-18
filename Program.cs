74f0253d976eb871048dae04bbf03feca179b1f4
Chitti-Srija <133970809+Chitti-Srija@users.noreply.github.com>
2023-05-18 17:11:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+Console.WriteLine("Hello, World");
+
+string fileName = @"C:\Users\ckomati\source\repos\multitasking\multitasking\bin\Debug\net6.0\srija.txt";
+FileInfo fi = new FileInfo(fileName);
+try
+{
+    if (fi.Exists)
+    {
+        fi.Delete();
+    }
+    using (StreamWriter sw = fi.CreateText())
+    {
+        sw.WriteLine("New file created: {0}", DateTime.Now.ToString());
+        sw.WriteLine("Author: Srija");
+        sw.WriteLine("Add one more line");
+        sw.WriteLine("Add one more line ");
+        sw.WriteLine("Done!");
+    }
+    using (StreamReader sr = File.OpenText(fileName))
+    {
+        string s = "";
+        while ((s = sr.ReadLine()) != null)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+        
+    
